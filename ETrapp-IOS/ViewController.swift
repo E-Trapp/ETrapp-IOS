21363bda49ce7ae87165bbee2ad2de72e6d4e11d
Josep B <ballber@gmail.com>
2017-11-17 00:27:40 +0100

    Initial Commit

---
+++ ETrapp-IOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ETrapp-IOS
+//
+//  Created by Joe on 17/11/17.
+//  Copyright © 2017 E-Trapp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
